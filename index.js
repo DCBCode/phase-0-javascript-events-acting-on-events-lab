14880a34fee4e585e31b400dec0afc39fa1f0493
Drew Benson <Drewbenson16@gmail.com>
2023-11-29 09:15:05 -0700

    first game

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+  
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    // Adjust the condition to prevent the dodger from moving off the screen
+    if (left + dodger.offsetWidth < window.innerWidth) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
